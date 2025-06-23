250704c932014fd8d4466366c9259349641c825c
Charles Silva <charlesk12019@gmail.com>
2025-06-23 10:30:59 -0300

    initial commit

---
+++ venv/Projeto/scripts.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".imagem-tenis")
+
+function mudarvisual(cor, imagem){
+    body.style.background = cor
+    tenis.src = imagem
+}
+
